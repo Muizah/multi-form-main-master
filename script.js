7ce374fcc45807e404de51336ac12bb0b9cfc1a7
Muizah <mapampa7@gmail.com>
2023-03-03 02:05:07 +0100

    work on functionality of buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+const step_one = document.getElementById("step_one_btn");
+const step_two = document.getElementById("step_two_btn");
+const step_three = document.getElementById("step_three_btn");
+const step_four = document.getElementById("step_four_btn");
+
